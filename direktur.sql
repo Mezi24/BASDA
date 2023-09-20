38d65e7c1711cff26dc344556227f618c7c055fe
FebriArdiansyah27 <131425009+FebriArdiansyah27@users.noreply.github.com>
2023-09-20 19:43:15 +0700

    Update direktur.sql

--- direktur.sql
+++ direktur.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 20, 2023 at 01:33 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `nasabah_bank`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `direktur`
---
 
 CREATE TABLE `direktur` (
   `id_direktur` int(30) NOT NULL,
@@ -36,16 +8,7 @@ CREATE TABLE `direktur` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
---
-
---
--- Indexes for table `direktur`
---
 ALTER TABLE `direktur`
   ADD PRIMARY KEY (`id_direktur`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
